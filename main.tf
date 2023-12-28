a63852aacbd5a7dbb0b59f986cc6af85b159bcc2
samrhim <rhimi1111@gmail.com>
2023-12-29 00:34:28 +0100

    first thing with ignore

--- main.tf
+++ main.tf
@@ -73,6 +73,5 @@ resource "aws_security_group" "dev_sg" {
 
 resource "aws_key_pair" "dev_key" {
   key_name   = "dev_key"
-  public_key = file(~/.ssh/mtckey.pub)
-  
+  public_key = file("~/.ssh/devkey.pub")
 } 
